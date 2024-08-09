6d79df47867015d0ef63f72b8f66651eb55333b5
Type-32 <87076491+Type-32@users.noreply.github.com>
2024-08-09 09:41:03 +0800

    Vault

--- src/main/java/cn/infinitumstudios/infinitumEconomy/foundation/types/Vault.java
+++ src/main/java/cn/infinitumstudios/infinitumEconomy/foundation/types/Vault.java
@@ -3,5 +3,46 @@ package cn.infinitumstudios.infinitumEconomy.foundation.types;
 import java.util.UUID;
 
 public class Vault {
-    private UUID vaultUUID;
+    private UUID vaultUUID, owner;
+    private String name;
+    private double value;
+
+    public Vault(UUID owner, String name, double value) {
+        this.vaultUUID = UUID.randomUUID();
+        this.owner = owner;
+        this.name = name;
+        this.value = value;
+    }
+
+    public UUID getVaultUUID() {
+        return vaultUUID;
+    }
+
+    public void setVaultUUID(UUID vaultUUID) {
+        this.vaultUUID = vaultUUID;
+    }
+
+    public UUID getOwner() {
+        return owner;
+    }
+
+    public void setOwner(UUID owner) {
+        this.owner = owner;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
 }
